5b74252ee9cb8dcb31833a9ba4c2a6f0a273c3d5
Jeremy Yap <jeremy.yapjl@gmail.com>
2016-09-05 20:51:31 +0800

    Initial commit

---
+++ generate.js
@@ -0,0 +1,80 @@
+var spies, redSpies, blueSpies;
+var spymasterCode;
+
+$(function() {
+	randomize();
+});
+
+function randomize() {
+	var usedWords = [];
+	var index;
+	$(".word").each(function() {
+		do {
+			var index = randInt(400);
+		} while ($.inArray(index, usedWords) != -1); 
+		usedWords.push(index);
+
+		$(this).html(words[index]);
+	})
+
+	spies = [];
+	redSpies = Math.random() > 0.5 ? 8 : 9;
+	blueSpies = redSpies == 9 ? 8 : 9;
+	for (var i=0; i<25; i++) { spies[i] = 0; }
+
+	var assassin = randInt(25);
+	spies[assassin] = 3;
+
+	for (var i=0; i<redSpies; i++) {
+		do {
+			index = randInt(25);
+		} while (spies[index] != 0);
+		spies[index] = 1;
+	}
+
+	for (var i=0; i<blueSpies; i++) {
+		do {
+			index = randInt(25);
+		} while (spies[index] != 0);
+		spies[index] = 2;
+	}
+
+	$(".card").each(function(index) {
+		var type = spies[index];
+		var className;
+		switch (type) {
+			case 1: className = "spy-red"; break;
+			case 2: className = "spy-blue"; break;
+			case 3: className = "spy-black"; break;
+		}
+		$(this).addClass(className);
+	})
+
+	$(".card").click(function() {
+		$(this).addClass("revealed");
+	})
+
+	spymasterCode = encodeSpymasterLink()
+}
+
+function randInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function encodeSpymasterLink() {
+	var base64chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+	var code = "";
+	var rot = randInt(14);
+	var acc = rot;
+
+	for (var i=0; i<25; i++) {
+		var index = (i + rot) % 25; 
+		acc = acc << 2;
+		acc += spies[index];
+		if ((i % 3) == 0) {
+			code += base64chars.charAt(acc);
+			acc = 0;
+		}
+	}
+	return code;
+}
---
+++ spymaster.js
@@ -0,0 +1,36 @@
+var spies, redSpies, blueSpies;
+var spymasterCode;
+
+
+$(function() {
+	spies = decodeSpymasterLink();
+
+	$(".card").each(function(index) {
+		var type = spies[index];
+		var className;
+		switch (type) {
+			case 1: className = "spy-red"; break;
+			case 2: className = "spy-blue"; break;
+			case 3: className = "spy-black"; break;
+		}
+		$(this).addClass(className);
+	})
+});
+
+function decodeSpymasterLink() {
+	var base64chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+	var code = window.location.href.slice(window.location.href.indexOf('?') + 1);
+	var arr = [];
+	for (var i=0; i<25; i++) { arr[i] = 0; }
+
+	var rot = base64chars.indexOf(code.charAt(0)) >> 2;
+
+	for (var i=0; i<9; i++) {
+		var value = base64chars.indexOf(code.charAt(i));
+		for (var j=0; j<3; j++) {
+			var index = (i * 3 + j - 2 + rot) % 25;
+			arr[index] = (value >> (4 - j * 2)) & 3;
+		}
+	}
+	return arr;
+}
