cbb3f0c2198e809b933dadebc532e13fc572b3a3
Yu Ming <sohymg@users.noreply.github.com>
2016-12-13 14:02:27 +0800

    Fix take anything you want video

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
 var videos = [
-  'https://www.youtube.com/embed/HRerwXWTRjM?start=35&autoplay=1',
+  'https://www.youtube.com/embed/rhTQrqyiHK0?start=27&autoplay=1',
   'https://www.youtube.com/embed/Ct6BUPvE2sM?autoplay=1',
   'https://www.youtube.com/embed/YVhxcpItk_M?autoplay=1&start=85'
 ];
